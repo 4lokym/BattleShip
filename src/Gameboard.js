6ed7119f2eb0bd06386381c7b434f10aeabebf6d
4lokym <mykola.koshilko@gmail.com>
2025-07-03 11:03:12 +0200

    TODO done, receive attack test done

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -19,10 +19,17 @@ export class Gameboard {
     return this;
   }
 
+  posValid(x, y) {
+    if (x >= this.grid.length || x < 0 || y >= this.grid[0].length || y < 0) {
+      return false;
+    }
+    return true;
+  }
+
   place(x, y, ship, horizontal = true) {
     if (
-      (horizontal && ship.length + x > this.grid.length) ||
-      (!horizontal && ship.length + y > this.grid[0].length)
+      (horizontal && !this.posValid(ship.length - 1 + x, y)) ||
+      (!horizontal && !this.posValid(x, ship.length - 1 + y))
     ) {
       throw new Error("Ship out of the board");
     } else {
@@ -30,22 +37,23 @@ export class Gameboard {
     }
   }
 
-// TODO 1) create a position check function, 2) add a position controll in atPos() & receiveAttack(), 
-// 3) Make unit test for receiveAttack() using a jest.mock Ship object (like explained in the video)
-
   atPos(x, y) {
-    return this.grid[x][y];
-  }
-
-  receiveAttack(x, y){
-    const cell = this.grid[x][y]
-    if(typeof this.grid[x][y] === "Ship"){
-      cell.hitIncr();
-      cell.updateSunk();
+    if (this.posValid(x, y)) {
+      return this.grid[x][y];
     }else{
-      this.grid[x][y] = {miss: true};
+      throw new Error("Coordinates out of the board");
     }
-    return this.grid[x][y];
   }
 
+  receiveAttack(x, y) {
+    const cell = this.grid[x][y];
+    if (!cell || cell.miss){
+      this.grid[x][y] = { miss: true };
+    }else if (cell.sunk){
+    }else {
+      cell.hitIncr();
+      cell.updateSunk()
+     }
+    return this.grid[x][y];
+  }
 }
--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,17 +1,28 @@
+import { experiments } from "webpack";
 import { Gameboard } from "../src/Gameboard";
 
 beforeEach(() => {
   aBoard = new Gameboard();
-  shipMock = jest.mock("../src/Ship", () => { return {horizontal: true, length: 1 }});
+  shipMock = jest.mock("../src/Ship", () => { return {horizontal: true, length: 1, sunk: false}});
+  shipMock.length = 1;
   [x, y] = [4, 4];
 });
 
-test("Place a ship and retrieve it", () => {
-  [x, y] = [1, 1];
-  expect(aBoard.place(x, y, shipMock).atPos(x, y)).toBe(shipMock);
+test("posValid: Is valid in a corner", () => {
+  [x, y] = [9, 9];
+  expect(aBoard.posValid(x, y)).toBe(true);
+  [x, y] = [10, 10];
+  expect(aBoard.posValid(x, y)).toBe(false);
 });
 
-test("Try to place a ship out of the board", () => {
+// here i've decided to check the test using the grid property to untie place() fn and atPos()
+test("place: Place a ship and retrieve it", () => {
+  [x, y] = [1, 1];
+
+  expect(aBoard.place(x, y, shipMock).grid[x][y]).toBe(shipMock);
+});
+
+test("place: Try to place a ship out of the board", () => {
   [x, y] = [11, 11];
   expect(() => {
     aBoard.place(x, y, shipMock);
@@ -24,12 +35,12 @@ test("Try to place a ship out of the board", () => {
   }).toThrow(Error);
 });
 
-test("Placing a ship in a corner", () => {
+test("place: Placing a ship in a corner", () => {
   [x, y] = [9, 9];
-  expect(aBoard.place(x, y, shipMock).atPos(x, y)).toBe(shipMock);
+  expect(aBoard.place(x, y, shipMock, shipMock.horizontal).grid[x][y]).toBe(shipMock);
 });
 
-test("Check ship is in every tile", () =>{
+test("place: Check ship is in every tile", () =>{
   [x, y] = [4, 4];
   shipMock.length = 3;
   expect(aBoard.place(x, y, shipMock).atPos(x, y)).toBe(shipMock);
@@ -38,7 +49,18 @@ test("Check ship is in every tile", () =>{
   expect(aBoard.place(x, y, shipMock).atPos(x+3, y)).not.toBe(shipMock);
 });
 
-test("Recieve attack", () => {
+test("receiveAttack: Recieve attack on ship and on miss", () => {
+  shipMock.hitIncr = jest.fn(() => {});
+  shipMock.updateSunk = jest.fn(() => {});
   aBoard.place(x, y, shipMock);
-  expect(aBoard.receiveAttack(x, y).sunk).toBe(true);
+  aBoard.receiveAttack(x, y);
+  expect(shipMock.hitIncr).toHaveBeenCalled();
+  aBoard.receiveAttack(x-1, y-1);
+  expect(shipMock.hitIncr.mock.calls.length).toBe(1);
 });
+
+test("receiveAttack: check gameboard tracks all ships sunk", () =>{
+  aBoard.place(x, y, shipMock);
+  aBoard.receiveAttack(x, y);
+  expect(aBoard.allSunk)
+})
